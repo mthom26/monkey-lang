8c292e439e9299808641a449075fd8759be4e9cd
mthom26 <mthom2626@gmail.com>
2019-09-29 19:21:43 +0100

    Check for keywords and identifiers in lexer

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,11 @@ pub fn lexer(input: &[u8]) -> Vec<Token> {
             break;
         }
         match input[pos] {
+            ch if is_letter(ch) => {
+                let (new_pos, token) = read_letters(pos, input);
+                tokens.push(token);
+                pos = new_pos;
+            },
             b'{' => tokens.push(Token::LBRACE),
             b'}' => tokens.push(Token::RBRACE),
             b'(' => tokens.push(Token::LPAREN),
@@ -51,9 +56,37 @@ pub fn lexer(input: &[u8]) -> Vec<Token> {
     tokens
 }
 
+// Currently number digits can't be used in identifiers
+// May change this later
+fn is_letter(ch: u8) -> bool {
+    b'a' <= ch && ch <= b'z' ||
+    b'A' <= ch && ch <= b'Z' ||
+    b'_' == ch
+}
+
+fn read_letters(start_pos: usize, input: &[u8]) -> (usize, Token) {
+    let mut pos = start_pos;
+    let mut identifier = Vec::new();
+    // Add next character to identifier until next character is not a letter
+    while pos < input.len() && is_letter(input[pos]) {
+        identifier.push(input[pos]);
+        pos += 1;
+    }
+
+    let token = is_keyword(&identifier);
+    (pos - 1, token)
+}
+
+fn is_keyword(chars: &[u8]) -> Token {
+    match chars {
+        [b'l', b'e', b't'] => Token::LET, // TODO Add more keywords
+        chars => Token::IDENT(String::from_utf8_lossy(chars).to_string())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::{lexer, Token};
+    use crate::{lexer, Token, is_letter};
 
     #[test]
     fn lex_tokens() {
@@ -71,5 +104,26 @@ mod tests {
         ];
 
         assert_eq!(expected, tokens);
+    }
+
+    #[test]
+    fn test_is_letter() {
+        let input = b'a';
+        assert!(is_letter(input));
+        let input = b'A';
+        assert!(is_letter(input));
+        let input = b'z';
+        assert!(is_letter(input));
+        let input = b'Z';
+        assert!(is_letter(input));
+        let input = b'f';
+        assert!(is_letter(input));
+        let input = b'U';
+        assert!(is_letter(input));
+        let input = b'_';
+        assert!(is_letter(input));
+
+        let input = b'&';
+        assert!(!is_letter(input));
     }
 }
