f74dac41a6e8a3c5f0ec21faa78b7252f4dffeb3
mthom26 <mthom2626@gmail.com>
2019-09-30 12:57:02 +0100

    Add basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use rustyline::{
+    self,
+    error::ReadlineError
+};
+
 mod lexer;
-use lexer::{lexer, Token};
+use lexer::{lexer};
 
 fn main() {
-    
+    let mut rl = rustyline::Editor::<()>::new();
+
+    loop {
+        match rl.readline(">> ") {
+            Ok(line) => {
+                let tokens = lexer(line.as_bytes());
+                println!("{:?}", tokens);
+            },
+            Err(ReadlineError::Interrupted) => break, // 'Ctrl-c' pressed
+            Err(_) => println!("No Input")
+        }
+    }
 }
