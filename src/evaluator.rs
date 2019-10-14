5deb131f3f250450765fedf3efd7906537c2dc3a
mthom26 <mthom2626@gmail.com>
2019-10-14 23:26:16 +0100

    Add evaluator

---
+++ src/evaluator.rs
@@ -0,0 +1,60 @@
+use crate::parser::{
+    Statement, Expression
+};
+
+#[derive(Debug, PartialEq)]
+pub enum Object {
+    Null,
+    Int(isize),
+    Boolean(bool),
+}
+
+pub fn eval(ast : Vec<Statement>) -> Object {
+    let mut result = Object::Null;
+
+    for statement in ast {
+        match statement {
+            Statement::ExpressionStatement(exp) => {
+                result = eval_expression(exp);
+            },
+            _ => ()
+        }
+    }
+
+    result
+}
+
+fn eval_expression(exp: Expression) -> Object {
+    match exp {
+        Expression::Int(val) => Object::Int(val),
+        Expression::Boolean(val) => Object::Boolean(val),
+        _ => panic!("Unexpected Expression in eval_expression")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        lexer::lexer,
+        parser::parse,
+        evaluator::{eval, Object}
+    };
+
+    // Convenience function to lex, parse and eval an input
+    fn evaluated(input: &str) -> Object {
+        let mut tokens = lexer(input.as_bytes());
+        let statements = parse(&mut tokens);
+        eval(statements)
+    }
+
+    #[test]
+    fn test_expression_eval() {
+        let input = "5";
+        let expected = Object::Int(5);
+        assert_eq!(expected, evaluated(input));
+
+        let input = "false";
+        let expected = Object::Boolean(false);
+        assert_eq!(expected, evaluated(input));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod lexer;
 use lexer::{lexer};
 mod parser;
 use parser::parse;
+mod evaluator;
+use evaluator::{eval, Object};
 
 fn main() {
     let mut rl = rustyline::Editor::<()>::new();
@@ -16,7 +18,13 @@ fn main() {
             Ok(line) => {
                 let mut tokens = lexer(line.as_bytes());
                 let ast = parse(&mut tokens);
-                println!("{:?}", ast);
+                let evaluated = eval(ast);
+
+                match evaluated {
+                    Object::Int(val) => println!("{}", val),
+                    Object::Boolean(val) => println!("{}", val),
+                    Object::Null => println!("Null")
+                }
             },
             Err(ReadlineError::Interrupted) => break, // 'Ctrl-c' pressed
             Err(_) => println!("No Input")
