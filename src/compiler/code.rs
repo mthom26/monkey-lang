0fbe63baf9070111e96b77bb71c9f0882e3f0b5c
mthom26 <mthom2626@gmail.com>
2019-11-04 18:51:37 +0000

    Add 'two_u8_to_usize' utility function

--- src/compiler/code.rs
+++ src/compiler/code.rs
@@ -6,18 +6,22 @@ pub fn make_op(opcode: OpCode) -> Vec<u8> {
     match opcode {
         OpCode::OpConstant(operand) => {
             let mut output = vec![0x01];
-            let int1 = (operand >> 8) as u8;
-            let int2 = operand as u8;
-            output.push(int1);
-            output.push(int2);
+            let int_one = (operand >> 8) as u8;
+            let int_two = operand as u8;
+            output.push(int_one);
+            output.push(int_two);
             output
         }
     }
 }
 
+pub fn two_u8_to_usize(int_one: u8, int_two: u8) -> usize {
+    ((int_one as usize) << 8) | int_two as usize
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::compiler::{make_op, OpCode};
+    use crate::compiler::{make_op, two_u8_to_usize, OpCode};
 
     #[test]
     fn make_op_constant() {
@@ -28,5 +32,16 @@ mod tests {
         let op = make_op(OpCode::OpConstant(4449));
         let expected = vec![0x01, 17, 97];
         assert_eq!(expected, op);
+    }
+
+    #[test]
+    fn test_two_u8_to_usize() {
+        let input = two_u8_to_usize(1, 1);
+        let expected = 257;
+        assert_eq!(expected, input);
+
+        let input = two_u8_to_usize(10, 77);
+        let expected = 2637;
+        assert_eq!(expected, input);
     }
 }
--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,4 +1,4 @@
 mod code;
 mod compiler;
-pub use code::{make_op, OpCode};
+pub use code::{make_op, two_u8_to_usize, OpCode};
 pub use compiler::{ByteCode, Compiler};
